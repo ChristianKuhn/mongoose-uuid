bbdd7fea0664d651f421155d900019824dacc2b1
Christian Kuhn <c.kuhn@qn-solutions.de>
2016-08-30 23:02:28 +0200

    version bump replace require with own implementation

--- index.js
+++ index.js
@@ -6,37 +6,42 @@ var mongoose = require('mongoose');
 var bson = require('bson');
 var util = require('util');
 var uuid = require('node-uuid');
-var isMongooseObject = require('../mongoose/lib/utils').isMongooseObject;
 
-function getter (binary){
-  if(!binary) return '';
-  var len = binary.length();
-
-  var b = binary.read(0,len);
-
-  var buf = new Buffer(len);
-
-  for (var i = 0; i < len; i++)
-    buf[i] = b[i];
-
-  var hex = '';
-
-  for (var i = 0; i < len; i++)
-  {
-    var n = buf.readUInt8(i);
-    if (n < 16)
-      hex += '0'+n.toString(16);
-    else
-      hex += n.toString(16);
-  }
-
-  var uuidStr = hex.substr(0, 8) + '-' + hex.substr(8, 4) + '-' + hex.substr(12, 4) + '-' + hex.substr(16, 4) + '-' + hex.substr(20, 12);
-  return uuidStr;
+function isMongooseObject(value) {
+	if (value instanceof Object && value._id)
+		return true;
+	else
+		return false;
 }
 
-function SchemaUUID(path, options){
-  mongoose.SchemaTypes.Buffer.call(this, path, options);
-  this.getters.push(getter);
+function getter(binary) {
+	if (!binary) return '';
+	var len = binary.length();
+
+	var b = binary.read(0, len);
+
+	var buf = new Buffer(len);
+
+	for (var i = 0; i < len; i++)
+		buf[i] = b[i];
+
+	var hex = '';
+
+	for (var i = 0; i < len; i++) {
+		var n = buf.readUInt8(i);
+		if (n < 16)
+			hex += '0' + n.toString(16);
+		else
+			hex += n.toString(16);
+	}
+
+	var uuidStr = hex.substr(0, 8) + '-' + hex.substr(8, 4) + '-' + hex.substr(12, 4) + '-' + hex.substr(16, 4) + '-' + hex.substr(20, 12);
+	return uuidStr;
+}
+
+function SchemaUUID(path, options) {
+	mongoose.SchemaTypes.Buffer.call(this, path, options);
+	this.getters.push(getter);
 }
 
 /*!
@@ -52,8 +57,8 @@ SchemaUUID.schemaName = 'UUID';
  * @api private
  */
 
-SchemaUUID.prototype.checkRequired = function (value) {
-  return value instanceof mongoose.Types.Buffer.Binary;
+SchemaUUID.prototype.checkRequired = function(value) {
+	return value instanceof mongoose.Types.Buffer.Binary;
 };
 
 /**
@@ -63,43 +68,45 @@ SchemaUUID.prototype.checkRequired = function (value) {
  * @api private
  */
 
-SchemaUUID.prototype.cast = function (value) {
-  if(isMongooseObject(value)) return value;
-  // If null or undefined
-  if (value == null || value === '')
-    return value;
+SchemaUUID.prototype.cast = function(value) {
 
-  if (value instanceof mongoose.Types.Buffer.Binary)
-    return value;
+	if (isMongooseObject(value)) return value;
 
-  var uuidBuffer;
+	// If null or undefined
+	if (value == null || value === '')
+		return value;
 
-  // support for timestamps
-  if (typeof value !== 'undefined') {
-    if (typeof value === 'string') {
-      // support for uuid strings
-      uuidBuffer = new mongoose.Types.Buffer(uuid.parse(value));
-      uuidBuffer.subtype(bson.SUBTYPE_UUID);
-      return uuidBuffer.toObject();
-    }
-  }
+	if (value instanceof mongoose.Types.Buffer.Binary)
+		return value;
 
-  throw new Error('Could not cast ' + value + ' to uuid.');
+	var uuidBuffer;
+
+	// support for timestamps
+	if (typeof value !== 'undefined') {
+		if (typeof value === 'string') {
+			// support for uuid strings
+			uuidBuffer = new mongoose.Types.Buffer(uuid.parse(value));
+			uuidBuffer.subtype(bson.SUBTYPE_UUID);
+			return uuidBuffer.toObject();
+		}
+	}
+
+	throw new Error('Could not cast ' + value + ' to uuid.');
 };
 
-SchemaUUID.prototype.castForQuery = function ($conditional, val) {
-  var handler;
-  if (arguments.length === 2) {
-    handler = this.$conditionalHandlers[$conditional];
-    if (!handler)
-      throw new Error("Can't use " + $conditional + " with Buffer.");
-    return handler.call(this, val);
-  } else {
-    val = $conditional;
-    return this.cast(val);
-  }
+SchemaUUID.prototype.castForQuery = function($conditional, val) {
+	var handler;
+	if (arguments.length === 2) {
+		handler = this.$conditionalHandlers[$conditional];
+		if (!handler)
+			throw new Error("Can't use " + $conditional + " with Buffer.");
+		return handler.call(this, val);
+	} else {
+		val = $conditional;
+		return this.cast(val);
+	}
 };
 
-module.exports.loadType = function loadType(mongoose){
-  mongoose.Types.UUID = mongoose.SchemaTypes.UUID = SchemaUUID;
+module.exports.loadType = function loadType(mongoose) {
+	mongoose.Types.UUID = mongoose.SchemaTypes.UUID = SchemaUUID;
 }
